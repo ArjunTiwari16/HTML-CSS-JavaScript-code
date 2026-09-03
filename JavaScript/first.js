889355f177cb7d1b9b5436afc1be0fee96503aee
Arjun Tiwari <at0397592@gmail.com>
2026-09-03 17:27:17 +0530

    HTML CSS JavaScript code

---
+++ JavaScript/first.js
@@ -0,0 +1,26 @@
+console.log("Welcome to Apana College");
+console.log("Mr Shyam ");
+let age = 24;
+age = 51;
+console.log(age);
+let fullname = "Tony Stark";
+console.log(fullname);
+let x = null;
+let y = undefined;
+console.log(y);
+let isFollow = true;
+const PI = 3.14;
+console.log(isFollow);
+let a = BigInt("123");
+console.log(a);
+let b = Symbol("Hello");
+console.log(b);
+const student = {
+    Name : "Rahul Kumar",
+    age : 50,
+    cgpa : 9.2,
+    isPass : true
+};
+console.log(student);
+student["Name"] = "Shyam";
+console.log(student["Name"]);
